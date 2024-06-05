10e81eeb8d70bc8b4e87657d1674dd5f0a1560cf
sevvalkanat <skanat93@gmail.com>
2024-06-05 15:18:49 +0300

    updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 
 
 //connect db
-mongoose.connect('mongodb://localhost/myPost-diary');
+mongoose.connect('mongodb://127.0.0.1:27017/myPost-diary');
 
 //template engine
 app.set("view engine","ejs");
